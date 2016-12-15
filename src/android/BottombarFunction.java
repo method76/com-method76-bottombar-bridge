b3a0e7f210dbd0d22a297dfe01ee795ada521e70
Sungjoon Kim <methodtweak@naver.com>
2016-12-15 11:12:56 +0900

    Create BottombarFunction.java

---
+++ src/android/BottombarFunction.java
@@ -0,0 +1,28 @@
+package com.method76.bottombar.bridge;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.cordova.CordovaActivity;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.text.Html;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaResourceApi;
+import org.apache.cordova.PluginResult;
+
+/**
+ * BottombarBridge is a Cordova plugin that communicates Native Tab UI with JavaScript
+ * @author methodtweak@naver.com
+ */
+interface BottombarFunction {
+
+    int changeTab(int idx, String URL);
+
+}
