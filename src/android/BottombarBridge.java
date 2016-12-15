df06bfdc619f9e0d26b7c61ec192bdb0b55d4b5f
Sungjoon Kim <methodtweak@naver.com>
2016-12-15 14:22:18 +0900

    Update BottombarBridge.java

--- src/android/BottombarBridge.java
+++ src/android/BottombarBridge.java
@@ -1,15 +1,11 @@
 package com.method76.bottombar.bridge;
 
-import org.apache.cordova.CordovaActivity;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import android.util.Log;
 
-import org.apache.cordova.CordovaActivity;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaResourceApi;
 import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
 
 /**
  * WebIntent is a PhoneGap plugin that bridges Android intents and web
@@ -27,27 +23,27 @@ public class BottombarBridge extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
 
-	if (action==null || "changeTab".equals(action)==false
-		|| args.length() < 1) {
-		//return new PluginResult(PluginResult.Status.INVALID_ACTION);
-		callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.INVALID_ACTION));
-		return false;
-	}
+        if (action==null || "changeTab".equals(action)==false
+            || args.length() < 1) {
+            //return new PluginResult(PluginResult.Status.INVALID_ACTION);
+            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.INVALID_ACTION));
+            return false;
+        }
 
         try {
             int tabIdx = args.getInt(0);
-	    String url = args.length()==2?args.getString(1):null;
+	        String url = args.length()==2?args.getString(1):null;
 
             Log.d("Tab clicked: ", tabIdx + "/" + url);
 
             // Todo: Some validation required
 
             BottombarFunction act = (BottombarFunction) this.cordova.getActivity();
-	    if (url==null) {
-		act.changeTab(tabIdx);
-	    } else {
-		act.changeTab(tabIdx, url);
-	    }
+            if (url==null) {
+                act.changeTab(tabIdx);
+            } else {
+                act.changeTab(tabIdx, url);
+            }
 
             //return new PluginResult(PluginResult.Status.OK);
             callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK));
